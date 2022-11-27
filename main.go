213caf6e742377cf8c65453f891f80652a0432a5
Horace Heaven <horaceheaven@Horaces-MacBook-Pro.local>
2022-11-27 10:36:03 -0500

    Add workflow

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
+var version = "master/unreleased"
+
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Println("")
-		return
+	eval := flag.String("eval", "", "Code to execute")
+	vers := flag.Bool("version", false, "Show version")
+
+	flag.Parse()
+
+	if *vers {
+		fmt.Printf("monkey %s\n", version)
+		os.Exit(1)
 	}
+
+	if *eval != "" {
+		// TODO: add execute func
+		os.Exit(1)
+	}
+
+	var input []byte
+	var err error
+
+	if len(flag.Args()) > 0 {
+		input, err = ioutil.ReadFile(os.Args[1])
+	} else {
+		input, err = ioutil.ReadAll(os.Stdin)
+	}
+
+	if err != nil {
+		fmt.Printf("Error reading: %s\n", err.Error())
+	}
+
+	// TODO: add execute
+	fmt.Println(input)
 }
