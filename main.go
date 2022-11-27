2d6d6e694fb4797dae3d5cfd912d18155f0986e3
Horace Heaven <horaceheaven@Horaces-MacBook-Pro.local>
2022-11-27 09:38:23 -0500

    Add workflow

--- main.go
+++ main.go
@@ -8,12 +8,6 @@ import (
 func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("")
-		repl.Start(os.Stdin, os.Stdout)
 		return
 	}
-
-	if err := runProgram(os.Args[1]); err != nil {
-		os.Exit(1)
-	}
-
 }
